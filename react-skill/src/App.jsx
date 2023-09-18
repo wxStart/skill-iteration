28947a9f8217d6e0ab319e40117ff00e1df3ce72
wangxing <xing.wang@xforwardai.com>
2023-09-18 15:51:52 +0800

    fix: event 事件处理

--- react-skill/src/App.jsx
+++ react-skill/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 import ModuleCss from './components/module-css/Index.jsx';
+import EventIndex from './components/event/Index.jsx';
 
 function App() {
   return (
     <>
       <ModuleCss></ModuleCss>
+      <EventIndex />
     </>
   );
 }
---
+++ react-skill/src/components/event/Index.jsx
@@ -0,0 +1,21 @@
+export default function EventIndex() {
+  const handClick1 = e => {
+    console.log('1e: ', e);
+  };
+  const handClick2 = num => {
+    return e => {
+      console.log('2e:num ', e, num);
+    };
+  };
+  const handClick3 = (e, num) => {
+    console.log('3e:num ', e, num);
+  };
+  return (
+    <>
+      <button onClick={handClick1}>点击1</button>
+      <button onClick={handClick2(123)}>点击2</button>
+      <button onClick={e => handClick3(e, 123)}>点击3</button>
+      <button onClick={handClick3}>点击4</button>
+    </>
+  );
+}
