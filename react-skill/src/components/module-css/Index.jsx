39f8beccc12d850a4f1b77dc6292256a533cf7fb
wangxing <xing.wang@xforwardai.com>
2023-09-18 15:41:44 +0800

    fix: 处理className 像对象一样的处理

--- react-skill/src/components/module-css/Index.jsx
+++ react-skill/src/components/module-css/Index.jsx
@@ -1,5 +1,6 @@
 import style from './index.module.css';
 import ScssStyle from './index.module.scss';
+import classNames from 'classnames';
 
 export default function Index() {
   return (
@@ -8,6 +9,14 @@ export default function Index() {
       <div className={style.headTitle}>headTitle</div>
       <div className={ScssStyle.head}>head scss</div>
       <div className={ScssStyle.headTitle}>headTitle scss</div>
+      <div
+        className={classNames({
+          [style.head]: true,
+          [ScssStyle.head]: true,
+        })}
+      >
+        组合 classNames
+      </div>
     </>
   );
 }
