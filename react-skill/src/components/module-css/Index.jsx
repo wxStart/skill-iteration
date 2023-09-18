a9a05c5b9b800e9cbc03c90b606b498c1894d89e
wangxing <xing.wang@xforwardai.com>
2023-09-18 15:10:01 +0800

    [FIX]:css样式处理

---
+++ react-skill/src/components/module-css/Index.jsx
@@ -0,0 +1,10 @@
+import style from './index.module.css';
+
+export default function Index() {
+  return (
+    <>
+      <div className={style.head}>head</div>
+      <div className={style.headTitle}>headTitle</div>
+    </>
+  );
+}
