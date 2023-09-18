5ccf45f34a4e26510a4cf8ade5cee355a8a1c55a
wangxing <xing.wang@xforwardai.com>
2023-09-18 15:15:48 +0800

    fix: css预处理scss

--- react-skill/src/components/module-css/Index.jsx
+++ react-skill/src/components/module-css/Index.jsx
@@ -1,10 +1,13 @@
 import style from './index.module.css';
+import ScssStyle from './index.module.scss';
 
 export default function Index() {
   return (
     <>
       <div className={style.head}>head</div>
       <div className={style.headTitle}>headTitle</div>
+      <div className={ScssStyle.head}>head scss</div>
+      <div className={ScssStyle.headTitle}>headTitle scss</div>
     </>
   );
 }
